1b866ac99e37d23a86be3c2eff0fb1aab6e07c22
Zonaid <ahammedzonaid@gmail.com>
2024-07-14 17:37:34 +0600

    collection panel close button onclick

--- Assets/Scripts/TutorialHelpButtonManager.cs
+++ Assets/Scripts/TutorialHelpButtonManager.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class TutorialHelpButtonManager : MonoBehaviour
 {
     // Start is called before the first frame update
     public Image tutorialImage;
+        // a textmesh pro header text
+    public TMP_Text headerText;
+    public TMP_Text bodyText;
 
     void Start()
     {
@@ -20,7 +24,13 @@ public class TutorialHelpButtonManager : MonoBehaviour
     }
 
     public void CollectionPanelHelpButton() {
+        headerText.text = "My Collection";
+        bodyText.text = "This is where you can view all the cards you have collected.";
         tutorialImage.gameObject.SetActive(true);
+    }
+
+    public void CollectionHelpPanelClose() {
+        tutorialImage.gameObject.SetActive(false);
     }
 
 
